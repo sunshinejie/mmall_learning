15d57ef3d6ab516b459b4daa368e03fd50e62b18
smile_24 <1603428353@qq.com>
2021-03-06 23:14:57 +0800

    User module - v1.0

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,20 @@
+package com.mmall.common;
+
+/**
+ * @Description: TODO
+ * @Author smile_24
+ * @Date: 2021/3/2 下午4:27
+ */
+public class Const {
+
+    public static final String CURRENT_USER="currentUser";
+
+    public static final String EMAIL="email";
+    public static final String USERNAME="username";
+
+    public interface Role{
+        int ROLE_CUSTOMER=0;
+        int ROLE_ADMIN=1;
+    }
+
+}
